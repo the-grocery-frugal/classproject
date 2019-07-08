c922e4645b71d9b73e9ff97345c9869e05ac8e98
jacobdshimer <jacob.d.shimer@gmail.com>
2019-07-07 20:58:56 -0400

    All but Delete functions work.  Error checking needs to be done.

--- TheGroceryFrugal/src/main/java/com/thegrocery/thegroceryfrugal/GUI/ModifyGroceryList.java
+++ TheGroceryFrugal/src/main/java/com/thegrocery/thegroceryfrugal/GUI/ModifyGroceryList.java
@@ -274,7 +274,7 @@ public class ModifyGroceryList extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ModifyRecipe(RecipeUtility.getRecipe("default"), UserUtility.getUser("default")).setVisible(true);
+                new ModifyRecipe(RecipeUtility.getRecipe("default"), UserUtility.getUser("system-user")).setVisible(true);
             }
         });
     }
