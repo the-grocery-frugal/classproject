ec2d4b1938da1fdab394f9ca093974098d1dbdaa
gbgus23 <gbgus23@company.com>
2019-07-09 22:28:23 +0700

    Bug fixes

--- TheGroceryFrugal/src/main/java/com/thegrocery/thegroceryfrugal/Models/GroceryList.java
+++ TheGroceryFrugal/src/main/java/com/thegrocery/thegroceryfrugal/Models/GroceryList.java
@@ -148,4 +148,9 @@ public class GroceryList {
     public void setTitle(String title) {
         this.title = title;
     }
+    
+    @Override
+    public String toString() {
+    	return title;
+    }
 }
