b2db4066c26d9856201b6b3a8abb017964be04f0
Amanda Kok <>
2019-07-14 13:56:28 -0500

    Forgot to delete some commented out code from previous commit's fixes.  Also updated some javadocs.

--- TheGroceryFrugal/src/main/java/com/thegrocery/thegroceryfrugal/GUI/NewIngredient.java
+++ TheGroceryFrugal/src/main/java/com/thegrocery/thegroceryfrugal/GUI/NewIngredient.java
@@ -1,7 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * File: GUI.java 
+ * Author: Milagros Sasieta 
+ * Date: 6/16/2019 
+ * Purpose: Create the UI for adding a new ingredient to a recipe.
  */
 package com.thegrocery.thegroceryfrugal.GUI;
 
